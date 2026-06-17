cc15d53300680fe37234c75aacaa88af0bc864de
Rajatjyoti-arch <rajatb.0307@gmail.com>
2026-06-17 22:32:49 +0530

    Improved auto_push_output.log,main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import socket
 import threading
-from queue
+from queue import Queue
 
 target = input("Enter the ip = ")
-    
+
 def portscan(port):
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,11 +11,3 @@ def portscan(port):
         return True
     except:
         return False 
-
-for port in range(1, 1024):
-    result = portscan(port)
-    if result:
-        print("Port {} is open!".format(port))
-    else:
-        print("Port {} is closed!".format(port))
-         
