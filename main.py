b76ee49c7ee1c01cf7eb9e4968c791d18c9130ed
Rajatjyoti-arch <rajatb.0307@gmail.com>
2026-06-17 22:52:10 +0530

    Adjusted auto_push_output.log,main.py

--- main.py
+++ main.py
@@ -9,10 +9,15 @@ open_ports = []
 def portscan(port):
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Add a timeout so it doesn't hang for minutes on closed/filtered ports
+        sock.settimeout(1)
         sock.connect((target, port))
         return True
     except:
-        return False 
+        return False
+    finally:
+        # Always close the socket
+        sock.close()
 
 def fill_queue(port_list):
     for port in port_list:
@@ -24,19 +29,21 @@ def worker():
         if portscan(port):
             print("Port {} is open!".format(port))
             open_ports.append(port)
+
 port_list = range(1, 1024)
 fill_queue(port_list)
 
 thread_list = []
 
-for i in range (10):
+for i in range(50): # Increased thread count to speed up the scan
     thread = threading.Thread(target=worker)
     thread_list.append(thread)
 
 for thread in thread_list:
     thread.start()
 
-print()
-    
+# Wait for all threads in the thread_list to finish before proceeding
+for thread in thread_list:
+    thread.join()
 
-    
+print("The open ports are = ", open_ports)
