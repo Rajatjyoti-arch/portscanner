0232de90b0b7373581a83d2ecb1f42a5447b9f50
Rajatjyoti-arch <rajatb.0307@gmail.com>
2026-06-17 21:49:56 +0530

    Tweaked auto_push_output.log,main.py

--- main.py
+++ main.py
@@ -11,4 +11,7 @@ def portscan(port):
         return False 
 
 for port in range(1, 1024):
-    result =
+    result = portscan(port)
+    if result == True:
+        print("Port " , port , " is open")
+         
